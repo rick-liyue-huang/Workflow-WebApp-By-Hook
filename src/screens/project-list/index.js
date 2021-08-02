5b0f74ca7de34cddbdd1cf7afdee768f336a2bea
rickhuang <rick.liyue.huang@gmail.com>
2021-08-02 13:28:07 +1000

    feat: add modified code by custom hooks

--- src/screens/project-list/index.js
+++ src/screens/project-list/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import * as qs from "qs";
 import { SearchPanel } from "./searchPanel";
 import { List } from "./list";
-import { cleanObject } from "../../utils";
+import { cleanObject, useMount, useDebounce } from "../../utils";
 
 // deal with the backend url compatability
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -14,24 +14,25 @@ export const ProjectListScreen = () => {
   });
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
+  const debouncedParam = useDebounce(param, 2000);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
       }
-    );
-  }, [param]);
+    });
+  }, [debouncedParam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, []);
+  });
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 export const cleanObject = (object) => {
@@ -10,3 +12,41 @@ export const cleanObject = (object) => {
   });
   return result;
 };
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    // after value is changed, the timer is working
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+    // the return function will works after useEffect called, so here it clear the previous timer.
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+/*
+const debounce = (func, delay) => {
+  let timer = undefined;
+  // closure
+  return (...param) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function () {
+      func(...param);
+    }, delay);
+  }
+}
+
+const log = debounce(() => console.log('call'), 5000);
+log();
+log();
+log();
+*/
