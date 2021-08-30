b81ac027079ae5de59cce3e6da703e85a36f35fd
rickhuang <rick.liyue.huang@gmail.com>
2021-08-30 18:53:54 +1000

    feat: define initialUser to resolve page refresh problem

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,6 +3,7 @@ import qs from "qs";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import { cleanObject, useMount, useDebounce } from "../../utils";
+import { useHttp } from "../../utils/http";
 
 export const ApiUrl = process.env.REACT_APP_API_URL;
 
@@ -12,22 +13,14 @@ export const ProjectListScreen = () => {
   const [list, setList] = useState([]);
   const debouncedParam = useDebounce(param, 1000);
 
+  const client = useHttp();
+
   useEffect(() => {
-    fetch(
-      `${ApiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${ApiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
@@ -47,3 +40,29 @@ export const ProjectListScreen = () => {
     });
   }, []);
 * */
+
+/**
+  useEffect(() => {
+    client('projects', {data: cleanObject(debouncedParam)}).then(setList);
+    
+    fetch(
+      `${ApiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+ */
+
+/**
+   useMount(() => {
+    client('users').then(setUsers);
+    fetch(`${ApiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+   */
