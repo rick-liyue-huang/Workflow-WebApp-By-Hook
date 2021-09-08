a905063cfc6a005a171c26c7f5dd76a4a3744528
rickhuang <rick.liyue.huang@gmail.com>
2021-09-08 17:31:55 +1000

    feat: keep the authenticated state on refreshing page

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import qs from "qs";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import { cleanObject, useDebounce } from "../../utils";
+import { cleanObject, useDebounce, useMount } from "../../utils";
+import { useHttp } from "../../utils/http";
 
 export const ApiUrl = process.env.REACT_APP_API_URL;
 
@@ -14,7 +15,25 @@ export const ProjectListScreens = () => {
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
   const debouncedParam = useDebounce(param, 2000);
+  const clients = useHttp();
 
+  useEffect(() => {
+    clients("projects", { data: cleanObject(debouncedParam) }).then(setList);
+  }, [debouncedParam]);
+
+  useMount(() => {
+    clients("users").then(setUsers);
+  });
+
+  return (
+    <>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </>
+  );
+};
+
+/*
   useEffect(() => {
     fetch(
       `${ApiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
@@ -24,7 +43,9 @@ export const ProjectListScreens = () => {
       }
     });
   }, [debouncedParam]);
+* */
 
+/*
   useEffect(() => {
     fetch(`${ApiUrl}/users`).then(async (response) => {
       if (response.ok) {
@@ -32,11 +53,4 @@ export const ProjectListScreens = () => {
       }
     });
   }, []);
-
-  return (
-    <>
-      <SearchPanel param={param} setParam={setParam} users={users} />
-      <List list={list} users={users} />
-    </>
-  );
-};
+* */
