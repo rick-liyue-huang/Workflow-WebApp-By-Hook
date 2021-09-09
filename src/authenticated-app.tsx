3f8187f8b14fef8d9b60609a90526f278181bd2b
rickhuang <rick.liyue.huang@gmail.com>
2021-09-09 16:19:22 +1000

    feat: using flex and grid to layout page

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,67 @@
 import React from "react";
 import { ProjectListScreens } from "./screens/project-list";
 import { useAuth } from "./context/auth-context";
+import styled from "@emotion/styled";
+import { Button } from "antd";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <>
-      <ProjectListScreens />
-      <button onClick={logout}>Logout</button>
-    </>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>Project</h3>
+          <h3>Account</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <Button onClick={logout}>Logout</Button>
+        </HeaderRight>
+      </Header>
+      {/*<Nav>nav</Nav>*/}
+      <Main>
+        <ProjectListScreens />
+      </Main>
+      {/*<Aside>aside</Aside>*/}
+      {/*<Footer>footer</Footer>*/}
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-areas:
+    "header header header"
+    "nav main aside"
+    "footer footer footer";
+  height: 100vh;
+  grid-gap: 2rem;
+`;
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  grid-area: main;
+`;
+const Nav = styled.nav`
+  grid-area: nav;
+`;
+const Aside = styled.aside`
+  grid-area: aside;
+`;
+const Footer = styled.footer`
+  grid-area: footer;
+`;
